34c19e878d2bbcc32efc3925cba79d7448276899
analopesdev <analopesdemorais@outlook.com>
2020-09-12 09:03:55 -0300

    receiving data from the front with req.body

--- routes.js
+++ routes.js
@@ -2,7 +2,11 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function (req, res){
-    return res.render("todolist/index")
+    return res.render('todolist/index')
+})
+
+routes.post('/index', function (req, res){
+    return res.send(req.body)
 })
 
 module.exports = routes
