dfbd19319988356f1faa6e3a0058e382ef467279
analopesdev <analopesdemorais@outlook.com>
2020-09-13 00:23:21 -0300

    finish routes, deleteTask

--- routes.js
+++ routes.js
@@ -6,9 +6,14 @@ routes.get('/', function (req, res){
     return res.render('tasks/index')
 })
 
+routes.get('/tasks', function (req, res){
+    return res.redirect('tasks/index')
+})
+
 routes.get('/index/:id', tasks.getTask)
 routes.post('/index', tasks.createTask)
 routes.get('/index/:id/edit', tasks.editTask);
 routes.put('/index', tasks.updateTask)
+routes.delete('/index', tasks.deleteTask)
 
 module.exports = routes;
--- tasks.js
+++ tasks.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const data = require("./data.json")
 
-//Show
+//show
 exports.getTask = function(req, res){
  const {id}  = req.params
 
@@ -16,10 +16,9 @@ exports.getTask = function(req, res){
    created_at: new Intl.DateTimeFormat("pt-BR").format(foundTask.created_at),
  }
 
- return res.render("tasks/show.njk", {task})
+ return res.render("tasks/index.njk", {task})
 
 }
-
 
 //post
 exports.createTask = function(req, res){
@@ -36,7 +35,6 @@ exports.createTask = function(req, res){
   const created_at = Date.now()
   const id = Number(data.tasks.length + 1)
   
-  
   data.tasks.push({
     id,
     created_at,
@@ -44,17 +42,14 @@ exports.createTask = function(req, res){
     description
   })
 
-  //verificar
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
     if(err) return res.send("Write file error!")
     return res.render("tasks/index.njk")
   })
 
-  //  return res.send(req.body)
 }
 
 //edit
-
 exports.editTask = function (req, res){
   const {id}  = req.params
 
@@ -67,7 +62,45 @@ exports.editTask = function (req, res){
   return res.render('tasks/edit', {task: foundTask})
 }
 
-
 //update
+exports.updateTask = function(req, res){
+  const {id}  = req.body
+  let index = 0
 
+  const foundTask = data.tasks.find(function(task, foundIndex){
+    if(id == task.id){
+      index = foundIndex
+      return true
+    }
+  })
+ 
+  if(!foundTask) return res.send("Task not found")
+
+  const task = {
+    ...foundTask,
+    ...req.body
+  }
+
+  data.tasks[index] = task
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if(err) return res.send("Write error!")
+    return res.redirect(`/index/${id}`)
+  })
+}
+
+exports.deleteTask = function(req, res){
+  const {id} = req.body
+  const filteredTasks = data.tasks.filter(function(task){
+    return task.id != id
+  })
+
+  data.tasks =filteredTasks
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if(err) return res.send("White file not found")
+    return res.render("tasks/index")
+  })
+
+}
 //delete
