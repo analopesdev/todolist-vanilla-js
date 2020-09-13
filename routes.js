8e1306bc840d0b8c18316bda612954da528aaf9a
analopesdev <analopesdemorais@outlook.com>
2020-09-12 23:43:30 -0300

    created updateTask, create router, insert again in data , create router

--- routes.js
+++ routes.js
@@ -3,10 +3,12 @@ const routes = express.Router()
 const tasks = require('./tasks')
 
 routes.get('/', function (req, res){
-    return res.render('tasks/index.njk')
+    return res.render('tasks/index')
 })
 
 routes.get('/index/:id', tasks.getTask)
 routes.post('/index', tasks.createTask)
+routes.get('/index/:id/edit', tasks.editTask);
+routes.put('/index', tasks.updateTask)
 
 module.exports = routes;
